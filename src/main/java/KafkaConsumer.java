a55790f71568b4d0550cc07884c74818545c0a2b
Sampath Dhananjaya <sampath.dhananjaya@cambio.lk>
2018-08-09 18:54:35 +0100

    Initial commit

---
+++ src/main/java/KafkaConsumer.java
@@ -0,0 +1,88 @@
+/**
+ * @author SampathD
+ * @created 09/08/2018
+ */
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+
+public class KafkaConsumer
+{
+  private final static String TOPIC = "test2";
+
+  private final static String BOOTSTRAP_SERVERS = "localhost:9092,localhost:9093,localhost:9094";
+
+  private static Consumer<Long, String> createConsumer()
+  {
+    final Properties props = new Properties();
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaExampleConsumer");
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+    // Create the consumer using props.
+    final Consumer<Long, String> consumer = new org.apache.kafka.clients.consumer.KafkaConsumer<>(props);
+
+    // Subscribe to the topic.
+    consumer.subscribe(Collections.singletonList(TOPIC));
+    return consumer;
+  }
+
+  public static void runConsumer() throws InterruptedException
+  {
+    final Consumer<Long, String> consumer = createConsumer();
+
+    final int giveUp = 50;
+    int noRecordsCount = 0;
+    int counter = 0;
+
+    while (true)
+    {
+      final ConsumerRecords<Long, String> consumerRecords = consumer.poll(1000);
+
+      if (consumerRecords.count() == 0)
+      {
+        noRecordsCount++;
+        if (noRecordsCount > giveUp)
+          break;
+        else
+          continue;
+      }
+      Iterator it = consumerRecords.iterator();
+      while (it.hasNext())
+      {
+        ConsumerRecord<Long, String> record = (ConsumerRecord<Long, String>) it.next();
+        counter++;
+      }
+
+      consumerRecords.forEach(record -> {
+        System.out.printf("%d,%s,%d,%d \n", record.key(), record.value(),
+            record.partition(), record.offset());
+      });
+
+      consumer.commitAsync();
+    }
+    System.out.println(counter);
+    consumer.close();
+    System.out.println("DONE");
+  }
+
+  public static void main(String[] args)
+  {
+    try
+    {
+      KafkaConsumer.runConsumer();
+    }
+    catch (InterruptedException e)
+    {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ src/main/java/KafkaProducer.java
@@ -0,0 +1,105 @@
+/**
+ * @author SampathD
+ * @created 09/08/2018
+ */
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class KafkaProducer
+{
+  private final static String TOPIC = "test2";
+
+  private final static String BOOTSTRAP_SERVERS = "localhost:9092,localhost:9093,localhost:9094";
+
+  private static Producer<Long, String> createProducer()
+  {
+    Properties props = new Properties();
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+    props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer");
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    return new org.apache.kafka.clients.producer.KafkaProducer<Long, String>(props);
+  }
+
+  public static void runProducerSync(final int sendMessageCount)
+  {
+    final Producer<Long, String> producer = createProducer();
+    long time = System.currentTimeMillis();
+
+    try
+    {
+      for (long index = time; index < time + sendMessageCount; index++)
+      {
+        final ProducerRecord<Long, String> record = new ProducerRecord<Long, String>(TOPIC, index,
+            "Hello Mom " + index);
+
+        RecordMetadata metadata = producer.send(record).get();
+
+        long elapsedTime = System.currentTimeMillis() - time;
+        System.out.printf("sent record(key=%s value=%s) " + "meta(partition=%d, offset=%d) time=%d\n", record.key(),
+            record.value(), metadata.partition(), metadata.offset(), elapsedTime);
+
+      }
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+    finally
+    {
+      producer.flush();
+      producer.close();
+    }
+  }
+
+  public static void runProducer()
+  {
+    final Producer<Long, String> producer = createProducer();
+    long time = System.currentTimeMillis();
+
+    long counter = 1;
+    try
+    {
+      while (true)
+      {
+        final ProducerRecord<Long, String> record = new ProducerRecord<>(TOPIC, counter, "Message-" + counter);
+        RecordMetadata metadata = producer.send(record).get();
+        long elapsedTime = System.currentTimeMillis() - time;
+        System.out.println(
+            "Sent to topic - key: " + record.key() + " value: " + record.value() + " partition: " + metadata.partition()
+                + " offset: " + metadata.offset() + " time: " + elapsedTime);
+        if (counter == 500)
+        {
+          break;
+        }
+        counter++;
+      }
+    }
+    catch (InterruptedException e)
+    {
+      e.printStackTrace();
+    }
+    catch (ExecutionException e)
+    {
+      e.printStackTrace();
+    }
+    finally
+    {
+      producer.flush();
+      producer.close();
+    }
+  }
+
+  public static void main(String[] args)
+  {
+    KafkaProducer.runProducer();
+  }
+
+}
